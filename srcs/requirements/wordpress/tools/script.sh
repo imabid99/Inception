ff60aaf9ae0f7e5a0acfe5ff1df691ce4ce69e88
Imad Abid <imabid@student.1337.ma>
2023-06-25 03:48:59 +0100

    my-inception

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -8,29 +8,21 @@ curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.pha
 chmod +x wp-cli.phar
 mv wp-cli.phar /usr/local/bin/wp
 mkdir -p /var/www/html/wop
-wp core download --path=/var/www/html/wop --allow-root
 cd /var/www/html/wop
 
-wp config create --dbhost=mariadb --dbuser=$DB_USER --dbpass=$DB_PASSWORD --dbname=$DB_NAME --path=/var/www/html/wop  --skip-check --allow-root
+wp core download --path=/var/www/html/wop --allow-root
+wp config create --dbhost=mariadb --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASSWORD  --path=/var/www/html/wop --skip-check --allow-root
 wp core install  --url=$WP_URL --title=$SITE_TITLE --admin_user=$ADMIN_USER --admin_password=$ADMIN_PASSWORD --admin_email=$ADMIN_EMAIL --path=/var/www/html/wop --skip-email --allow-root
-
 wp user create $MY_USER $MY_EMAIL --role=author --user_pass=$MY_PASSWORD --path=/var/www/html/wop --allow-root
-
 wp theme install oceanwp --activate --path=/var/www/html/wop --allow-root
 
 wp plugin install redis-cache --activate --path=/var/www/html/wop --allow-root
-
 wp config set WP_REDIS_HOST 'redis' --path=/var/www/html/wop --allow-root
 wp config set WP_REDIS_PORT '6379' --path=/var/www/html/wop --allow-root
-wp config set WP_REDIS_DATABASE '0' --path=/var/www/html/wop --allow-root
-# wp config set WP_CACHE_KEY_SALT "$KEY_SALT" --path=/var/www/html/wop --allow-root
 wp redis enable --path=/var/www/html/wop --allow-root
-
-
-
-
 chown -R www-data:www-data /var/www/html/wop
 chmod -R 755 /var/www/html/wop
+
 
 sed -i 's/\/run\/php\/php7.3-fpm.sock/ 9000/g' /etc/php/7.3/fpm/pool.d/www.conf
 
